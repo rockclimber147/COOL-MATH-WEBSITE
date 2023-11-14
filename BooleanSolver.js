e0dea79fb5a993b542bcb2492788a4510e069194
Daylen Smith <daylencsmith@gmail.com>
2023-11-13 22:17:44 -0800

    Make Tokenizer use less memory

--- BooleanSolver.js
+++ BooleanSolver.js
@@ -1,6 +1,12 @@
 /**
- * NOTES
- * OPERATOR PRECEDENCES:
+ * Language GRAMMAR:
+ * 
+ * expression = Term (op Term)? -> One or more terms separated by BINARY OPERATORS
+ *  
+ * Term = !* Value (1 or 0)     -> A value preceded by 0 or more unary operators
+ *        ( Expression )        -> Another expression inside parentheses
+ *        
+ * Handling OPERATOR PRECEDENCE
  * 
  * parentheses () 4
  * Not          ! 3
@@ -36,14 +42,13 @@
  *     / \
  *    B   C
  * 
- *  Language GRAMMAR:
+ * COLORING EXPRESSIONS:
  * 
- * expression = Term (op Term)? -> One or more terms separated by BINARY OPERATORS
- * Term = 
- *        !* Value (1 or 0)  -> A value preceded by 0 or more unary operators
- *        ( Expression )     -> Another expression inside parentheses
- *        
- * 
+ * Example expression:
+ * 1+0*(1*!0+1)
+ * 1+
+ *   0*
+ *     (1*!0+1)
  */
 
 
@@ -71,7 +76,6 @@ class Tokenizer {
     debug = false;
     binaryOperators = ['*', '+'];
     values = ['0', '1'];
-    parentheses = ['(', ')'];
 
     inputString;
     stringIndex = 0;
@@ -91,17 +95,21 @@ class Tokenizer {
         let tokenType;
 
         if (currentChar == '!') {
-            tokenType = 'unaryOperator'
+            tokenType = 'unaryOperator';
         } else if (this.binaryOperators.includes(currentChar)) {
-            tokenType = 'binaryOperator'
+            tokenType = 'binaryOperator';
         } else if (this.values.includes(currentChar)) {
-            tokenType = 'binaryConstant'
-        } else if (this.parentheses.includes(currentChar)) {
-            tokenType = 'parenthesis'
+            tokenType = 'binaryConstant';
+        } else if (currentChar == '(') {
+            tokenType = 'openParenthesis';
+        } else if (currentChar == ')') {
+            tokenType = 'closeParenthesis';
         } else {
-            throw new Error(`unknown symbol: ${currentChar} at position ${i}`);
+            throw new Error(`unknown symbol: ${currentChar} at position ${this.stringIndex}`);
         }
         this.debugLog(`${currentChar}: ${tokenType}`);
+
+        this.stringIndex++;
         return new Token(tokenType, currentChar)
     }
 
@@ -142,7 +150,7 @@ class UnaryNode {
         if (nodeValue != '!') {
             throw new Error(`UnaryNode contains operator other than '!': ${nodeValue}`)
         }
-        this.nodeValue = nodeValue;
+        this.operator = nodeValue;
     }
     evaluate() {
         if (this.value == '1') {
@@ -150,12 +158,15 @@ class UnaryNode {
         }
         return '1';
     }
+    addChild(node){
+        this.child = node;
+    }
 }
 
 class TerminalNode {
     value;
     constructor(value) {
-        if (value != '1' || value != '0') {
+        if (value != '1' && value != '0') {
             throw new Error(`TerminalNode contains illegal value: ${value}`)
         }
         this.value = value;
@@ -167,14 +178,40 @@ class TerminalNode {
 
 class Parser {
     tokenizer;
+    currentToken;
     operatorPrecedences = {
-        '!': 3,
         '*': 2,
         '+': 1
     }
     tokenArray;
+
     constructor(expressionString, debug) {
-        let tokenizer = new Tokenizer(debug);
-        this.tokenArray = tokenizer.tokenize(expressionString);
+        // load tokenizer
+        this.tokenizer = new Tokenizer(expressionString, debug);
+    }
+    /**
+     * 
+     */
+    parseExpression(previousPrecedence){
+
+    }
+
+    /**
+     * Implements the
+     */
+    parseUnaryTerm(){
+        this.currentToken = this.tokenizer.advance();
+        switch (this.currentToken.tokenType){
+            case 'unaryOperator': {
+                let childNode = new UnaryNode(this.currentToken.lexeme);
+                childNode.addChild(this.parseUnaryTerm());
+                return childNode;
+            } case 'binaryConstant': {
+                return new TerminalNode(this.currentToken.lexeme);
+            }
+        }
     }
 }
+
+let parser = new Parser('!!!!!!1', true);
+console.log(parser.parseUnaryTerm())
